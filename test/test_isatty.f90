74c4018285a46eb5eee402d0cf4384848a2161c5
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-05-30 15:20:42 +0900

    [update] CMakeLists.txt

--- test/test_isatty.f90
+++ test/test_isatty.f90
@@ -1,12 +1,8 @@
 program main
-  use mod_unittest
   use fsys
   implicit none
-  type(unittest) :: u
 !
-  call u%init('test_fsys')
   print *, isatty()
-  call u%finish_and_terminate()
 !
 end program main
 
