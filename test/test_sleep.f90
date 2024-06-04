18c2df7ff964fd7e116f2f43f89adbd6312352d7
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-06-04 17:44:02 +0900

    [update] test_sleep.f90

--- test/test_sleep.f90
+++ test/test_sleep.f90
@@ -10,7 +10,7 @@ program main
   call SYSTEM_CLOCK(count=count_e)
   time = real(count_e - count_s, REAL64) / count_rate
   print'(2(A,f16.9))', "Sleep failed : system clock = ", time, " target = ", 0.1_REAL64
-  if (ABS(time - 0.1_REAL64) > 0.15_REAL64) then
+  if (ABS(time - 0.1_REAL64) > 0.20_REAL64) then ! 0.0 ~ 0.3
     error stop
   end if
 !
